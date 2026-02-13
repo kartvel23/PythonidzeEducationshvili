643f8c0889efe8b2130781c1755f5a383198ccb5
kartvel23 <berger_2323@mail.ru>
2026-02-14 00:51:15 +0300

    Управляемые_потоки2_Бергер_ДГ.py

---
+++ Управляемые_потоки2_Бергер_ДГ.py
@@ -0,0 +1,11 @@
+'''bool_variable=true 
+print(bool_variable)
+Ошибка которую получили: name 'true' is not defined. Did you mean: 'True'?'''
+bool_variable='true'
+print(bool_variable)
+print(type (bool_variable))
+'''Тип данных string, т.к. мы заключили true в кавычки; 
+python чувствителен к регистру и True≠true'''
+bool_variable_2=True
+print(bool_variable_2)
+print(type (bool_variable_2))
