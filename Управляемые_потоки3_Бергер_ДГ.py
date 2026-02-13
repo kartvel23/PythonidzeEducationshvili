a8b44bd3fb398ee3c0ace30558a02d6bd343c43c
kartvel23 <berger_2323@mail.ru>
2026-02-14 01:18:05 +0300

    Create Управляемые_потоки3_Бергер_ДГ.py

---
+++ Управляемые_потоки3_Бергер_ДГ.py
@@ -0,0 +1,15 @@
+user_name=input('Введите имя')
+Dmitriy_check='Дмитрий, твое рабочее место находится в другой комнате. Отойди от чужого компьютера и займись работой!'
+check_success='Добро пожаловать'
+if user_name=='Дмитрий':
+  print(Dmitriy_check)   
+if user_name!='Дмитрий':  
+ print(check_success)
+user_name=input('Введите имя')
+if user_name=='Дмитрий':
+ print(Dmitriy_check)   
+if user_name!='Дмитрий':
+  print(check_success)
+
+ 
+
